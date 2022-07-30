258952836f3febe2a3a6be2a29340ca358fda1ff
Violaine Romagné <romagne.v@gmail.com>
2022-07-30 15:40:52 +0200

    [FEATURE] Extended mode

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const toggleInfo = document.querySelector('.more');
+const info = document.querySelector('#info');
+
+toggleInfo.addEventListener('click', function () {
+    const open = JSON.parse(toggleInfo.getAttribute('aria-expanded'));
+    toggleInfo.setAttribute('aria-expanded', !open);
+    info.hidden = !info.hidden;
+
+    if (!info.hidden) {
+        toggleMenu.setAttribute('aria-label', 'Close info');
+
+    } else {
+        toggleInfo.setAttribute('aria-label', 'Open info');
+    }
+})
